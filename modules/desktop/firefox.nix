ddccff835f4c8d2c2dedd65ef6955df80d1d0957
Carlos Henrique <ch.henriquevinha@gmail.com>
2026-09-02 15:52:52 -0300

    [programs]: more configs firefox

--- modules/desktop/firefox.nix
+++ modules/desktop/firefox.nix
@@ -5,12 +5,17 @@
     enable = true;
 
     profiles.default = {
+      search =  {
+        force = true;
+        default = "DuckDuckGo";
+        privateDefault = "DuckDuckGo";
+      };
+
       isDefault = true;
       extensions.force = true;
       extensions.packages =
         with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
           ublock-origin
-          sponsorblock
           dearrow
           vimium
           privacy-badger
@@ -18,7 +23,20 @@
           stylus
           wayback-machine
           archivebox-exporter
+          bitwarden
         ];
+    };
+
+    policies = {
+      AppAutoUpdate                 = false;
+      BackgroundAppUpdate           = false;
+
+      DisableTelemetry = true;
+
+      # Access Restrictions
+      BlockAboutConfig              = false;
+      BlockAboutProfiles            = true;
+      BlockAboutSupport             = true;
     };
   };
 }
